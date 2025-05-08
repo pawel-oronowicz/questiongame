3ea88efc67e2aa5fba32c3b96d62a84d65c5b6f2
Paweloronowicz <oronowicz@gmail.com>
2025-05-08 23:31:09 +0200

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: import.meta.env.PORT || 4173,
+    // 👇 Tu dodajesz swoją domenę Rendera
+    allowedHosts: ['question-game-frontend.onrender.com']
+  }
 })
